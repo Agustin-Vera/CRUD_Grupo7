16df06a935ee78d4db65f52c0400f49226c88dbb
Agustin Vera <agustin.vera@usach.cl>
2024-08-10 20:05:42 -0400

    Se agrega el proyecto inicial Spring y Gradle Groovy

---
+++ ProyectoDBDGrupo7/src/main/java/com/example/ProyectoDBDGrupo7/ProyectoDbdGrupo7Application.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoDBDGrupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDbdGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDbdGrupo7Application.class, args);
+	}
+
+}
---
+++ ProyectoDBDGrupo7/src/test/java/com/example/ProyectoDBDGrupo7/ProyectoDbdGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoDBDGrupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDbdGrupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
